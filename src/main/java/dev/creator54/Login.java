dd2dc60cce460d72d18c42d0c785b09cd4948d65
creator54 <hi.creator54@gmail.com>
2023-07-16 22:39:01 +0530

    Added qkart testCases

---
+++ src/main/java/dev/creator54/Login.java
@@ -0,0 +1,65 @@
+package dev.creator54;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Login {
+    RemoteWebDriver driver;
+    String url = "https://crio-qkart-frontend-qa.vercel.app/login";
+    WebDriverWait wait = null;
+
+    public Login(RemoteWebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void navigateToLoginPage() {
+        if (!this.driver.getCurrentUrl().equals(this.url)) {
+            this.driver.get(this.url);
+        }
+    }
+
+    public Boolean PerformLogin(String Username, String Password) throws InterruptedException {
+        // Find the Username Text Box
+        WebElement username_txt_box = this.driver.findElement(By.id("username"));
+
+        // Enter the username
+        username_txt_box.sendKeys(Username);
+
+        // Find the password Text Box
+        WebElement password_txt_box = this.driver.findElement(By.id("password"));
+
+        // Enter the password
+        password_txt_box.sendKeys(Password);
+
+        // Find the Login Button
+        WebElement login_button = driver.findElement(By.className("button"));
+
+        // Click the login Button
+        login_button.click();
+
+        wait.until(ExpectedConditions.invisibilityOf(login_button));
+
+        return this.VerifyUserLoggedIn(Username);
+    }
+
+    public Boolean VerifyUserLoggedIn(String Username) {
+        try {
+            // Find the username label (present on the top right of the page)
+            WebElement username_label = wait.until (ExpectedConditions.visibilityOfElementLocated(By.className("username-text")));
+            return username_label.getText().equals(Username);
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+}
